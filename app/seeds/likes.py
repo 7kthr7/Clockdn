69f909b0075b828a83a864b5e9421374c23965ad
7kthr7 <mohamud.kawthar@gmail.com>
2023-08-09 04:57:25 -0500

    seeds and migrations

--- app/seeds/likes.py
+++ app/seeds/likes.py
@@ -23,14 +23,12 @@ def seed_likes():
     Like(user_id=8, post_id=3),
     Like(user_id=9, post_id=2),
     Like(user_id=10, post_id=1),
-    Like(user_id=2, post_id=8),
     Like(user_id=3, post_id=7),
     Like(user_id=4, post_id=6),
     Like(user_id=5, post_id=5),
     Like(user_id=6, post_id=4),
     Like(user_id=7, post_id=3),
     Like(user_id=8, post_id=2),
-    Like(user_id=9, post_id=1),
     ]
 
     for like in likes:
