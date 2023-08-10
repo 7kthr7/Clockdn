beebc9516408bcb7dcaf1b317880150bff0b5ed6
7kthr7 <mohamud.kawthar@gmail.com>
2023-08-09 23:57:49 -0500

    user feature backend complete

--- app/api/user_routes.py
+++ app/api/user_routes.py
@@ -1,8 +1,25 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 from flask_login import login_required
-from app.models import User
+from app.models import User, db
+# from app.forms import EditUserForm
+
+from .aws_helpers import get_unique_filename, upload_file_to_s3, remove_file_from_s3
+
 
 user_routes = Blueprint('users', __name__)
+
+
+
+def validation_errors_to_error_messages(validation_errors):
+    """
+    Simple function that turns the WTForms validation errors into a simple list
+    """
+    errorMessages = []
+    for field in validation_errors:
+        for error in validation_errors[field]:
+            errorMessages.append(f'{field} : {error}')
+    return errorMessages
+
 
 
 @user_routes.route('/')
@@ -25,9 +42,96 @@ def user(id):
     return user.to_dict()
 
 
-# @user_routes.route('/<int:id>/edit')
+# @user_routes.route('/update/<int:id>', methods=['PUT'])
 # @login_required
-# def user(id):
+# def edit_user(id):
+
+#     form = EditUserForm()
+#     print('UPDATE FORM DATA 2222222--->', form.data)
+#     form['csrf_token'].data = request.cookies['csrf_token']
+#     print('UPDATE FORM DATA 111111--->', form.data)
+    
+    
+#     if id > 10 and form.validate_on_submit():
+        
+#         user = User.query.get(id)
+        
+#         image = form.data["profile_image"]
+#         image.filename = get_unique_filename(image.filename)
+#         upload = upload_file_to_s3(image)
+        
+#         if "url" not in upload:
+#             return {'error': upload['errors']}
+
+#         if user.id:
+#             user.first_name=form.data['first_name']
+#             user.last_name=form.data['last_name']
+#             user.email=form.data['email']
+#             user.city=form.data['city']
+#             user.state=form.data['state']
+#             user.occupation=form.data['occupation']
+#             user.biography=form.data['biography']
+#             user.profile_image=upload['url']
+#             user.password=form.data['password']
+
+#             print("------->", user.id, user.first_name)
+            
+#             db.session.commit()
+#             return user.to_dict()
+#         print('UPDATED FORM DATA--->!!!!!!!', form.data)
+        
+#         print('INVALID FORM DATA--->!!!!!!!', form.errors)
+#         return {'errors': validation_errors_to_error_messages(form.errors)}, 401
+#         print('INVALID FORM DATA--->!!!!!!!', form.data)
+       
+
+@user_routes.route('/<int:id>', methods=['PUT'])
+@login_required
+def edit_user(id):
+    user = User.query.get(id)
+    data = request.form
+    first_name = data.get('first_name')
+    last_name = data.get('last_name')
+    profile_image = request.files.get('profile_image')
+    occupation = data.get('occupation')
+    biography = data.get('biography')
+    city = data.get('city')
+    state = data.get('state')
+
    
-#     user = User.query.get(id)
-#     return user.to_dict()
+
+    if user:
+        if profile_image:
+            if user.profile_image:
+                remove_file_from_s3(user.profile_image)
+
+            # image = data['profile_image']
+            profile_image.filename = get_unique_filename(profile_image.filename)
+            upload = upload_file_to_s3(profile_image)
+
+            if "url" not in upload:
+                return {'error': upload['errors']}
+
+            user.profile_image = upload['url']
+
+        user.first_name = first_name
+        user.last_name = last_name
+        user.occupation = occupation
+        user.biography = biography
+        user.city = city
+        user.state = state
+       
+        db.session.commit()
+        return user.to_dict()
+    return {'Message': 'User Information was successfully updated'}
+
+@user_routes.route('/<int:id>', methods=['Delete'])
+@login_required
+def delete_user(id):
+    user = User.query.get(id)
+    if user.id:
+
+        db.session.delete(user)
+    db.session.commit()
+
+    return {'User Successfully Deleted': id}
