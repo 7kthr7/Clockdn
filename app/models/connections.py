03c6f316e90ae1cf6c6f9c242be7e7b61f13b5fe
7kthr7 <mohamud.kawthar@gmail.com>
2023-08-11 13:09:51 -0500

    connections table bug

--- app/models/connections.py
+++ app/models/connections.py
@@ -1,27 +1,30 @@
 from .db import db, add_prefix_for_prod, environment, SCHEMA
 from datetime import datetime
 
-connections = db.Table('connections',
+connections = db.Table(
+    'connections',
     db.Model.metadata,
     db.Column('follower_id', db.Integer, db.ForeignKey(add_prefix_for_prod('users.id')), primary_key=True),
     db.Column('followed_id', db.Integer, db.ForeignKey(add_prefix_for_prod('users.id')), primary_key=True),
     db.Column("created_at", db.DateTime, default=datetime.now)
 )
-def to_dict_connections(self):
-        return {
-            'id': self.id,
-            'first_name': self.first_name,
-            'last_name': self.last_name,
-            'email': self.email,
-            'city': self.city,
-            'state': self.state,
-            'occupation': self.occupation,
-            'biography': self.biography,
-            'profile_image': self.profile_image,
-            'created_at': self.created_at.strftime('%B %d, %Y %I:%M %p'),
-            'updated_at': self.updated_at.strftime('%B %d, %Y %I:%M %p')
-            
-        }
 
-    #     if environment == "production":
-    # connections.schema = SCHEMA
+if environment == "production":
+    connections.schema = SCHEMA
+
+
+# def to_dict_connections(self):
+#         return {
+#             'id': self.id,
+#             'first_name': self.first_name,
+#             'last_name': self.last_name,
+#             'email': self.email,
+#             'city': self.city,
+#             'state': self.state,
+#             'occupation': self.occupation,
+#             'biography': self.biography,
+#             'profile_image': self.profile_image,
+#             'created_at': self.created_at.strftime('%B %d, %Y %I:%M %p'),
+#             'updated_at': self.updated_at.strftime('%B %d, %Y %I:%M %p')
+            
+#         }
