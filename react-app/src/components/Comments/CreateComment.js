13c441d86b478d176f4cc4d5ceae124e037063f5
7kthr7 <mohamud.kawthar@gmail.com>
2023-08-18 20:15:50 -0500

    cleaned up code

--- react-app/src/components/Comments/CreateComment.js
+++ react-app/src/components/Comments/CreateComment.js
@@ -1,86 +1,58 @@
 import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { createCommentThunk, getCommentsThunk } from '../../store/comment';
-import { useModal } from '../../context/Modal';
 import './CreateComment.css'
 
 
 const CreateComment = ({ postId, allComments }) => {
     const dispatch = useDispatch();
-    const { closeModal } = useModal()
     const [body, setBody] = useState('')
     const [frontendErrors, setFrontendErrors] = useState({})
 
+
+    // retrieving state from redux. search the all post state for a post with the same postId prop that's passed in. 
     const user = useSelector(state => state.session.user)
-
-
     const posts = Object.values(useSelector((state) => state.post.allPosts))
     const postDetail = posts.find((post) => post.id === postId)
+
+
 
     console.log('SINGLE POST FROM ALL POST STATE-->', postDetail)
     console.log('POST ID AND OCCUPATION OF SINGLE POST -->', postId)
 
+    //useEffect monitoring changes to the body variable. the error conditional re-renders when ever body changes.
     useEffect(() => {
         const frontendErrors = {}
-
-
-        if (body.length > 1000) {
-            frontendErrors.body = "Comment must be less than 100 characters"
+        if (body.length > 500) {
+            frontendErrors.body = "Comment must be less than 1000 characters"
         }
-        
-
         setFrontendErrors(frontendErrors)
     }, [body])
 
-    let disable = body.length === 0 || body.length > 1000;
+    //if the body count is 0 or more than 1000 characters don't show the submit button
+    let disable = body.length === 0 || body.length > 500;
 
-
-    useEffect(() => {
-        // Function to adjust textarea height
-        const handleTextareaInput = function() {
-            this.style.height = 'auto';           // Reset height to auto to shrink if text is removed
-            this.style.height = (this.scrollHeight) + 'px';  // Set the height to the content's scroll height
-        };
-        
-        const textareaElement = document.querySelector('.comment-textarea');
-        if (textareaElement) {
-            textareaElement.addEventListener('input', handleTextareaInput);
-        }
-    
-        //Cleanup
-        return () => {
-            if (textareaElement) {
-                textareaElement.removeEventListener('input', handleTextareaInput);
-            }
-        };
-    }, []); 
-
-
+    //async event handler that executes when the form is submitted while not causing a full page refresh thanks to e.preventDefault
     const handleSubmit = async (e) => {
         e.preventDefault();
 
+        //form being sent back to the backend to save in the db using thunks from the redux store
+        // dispatch all comments so the comment shows up and set body to empty string so that it clears the text area
         const newComment = new FormData();
-
         newComment.append('body', body);
-       
-
-        await setBody("");
-    dispatch(createCommentThunk( postDetail.id, newComment,));
-
+        dispatch(createCommentThunk(postDetail.id, newComment));
+        dispatch(getCommentsThunk())
+        setBody("");
     };
-   
-    
 
     return (
         <div className="comment-form-wrapper">
             <form method='POST' encType='multipart/form-data' onSubmit={handleSubmit}>
-
                 <div className='comment-input-wrapper'>
                 <img
                     src={user.profile_image}
                     style={{ width: "35px", height: "35px",borderRadius: "100%"}}
-                />
-                
+                />                
                 <label className="comment-label">
                     <textarea
                         className="comment-textarea"
@@ -91,8 +63,7 @@ const CreateComment = ({ postId, allComments }) => {
                         placeholder='Add a comment...'
                     />
                 </label>
-                </div>
-    
+                </div>  
                 {body && <button disabled={disable} className="submit-comment" type='submit'>Create Comment</button>}
             </form>
         </div>
